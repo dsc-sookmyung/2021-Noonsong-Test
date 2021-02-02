2f68e019ce649484eb22ea0dc5187697b70a3996
mori8 <kaithape@gmail.com>
2021-02-03 04:54:43 +0900

    Set router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import SelectContainer from './components/Chat/SelectContainer';
-import Select from './components/Chat/Select';
-import SpeechBubble from './components/Chat/SpeechBubble';
-import SpeechBubbleContainer from './components/Chat/SpeechBubbleContainer';
-import ProgressBar from './components/_Basic/ProgressBar';
+import Main from './components/Main/Main';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const pointColors = ['#4379B7', '#0D2D84']; // 연한 파랑, 코발트 블루
-
-const AppBlock = styled.div`
-  width: 70%;
-  margin: 0 auto;
-  margin-top: 4rem;
-  border: 1px solid black;
-  padding: 1rem;
-`;
+// const pointColors = ['#4379B7', '#0D2D84']; // 연한 파랑, 코발트 블루
 
 function App() {
   return (
-    <AppBlock>
-      <SpeechBubbleContainer>
-        <SpeechBubble role="question">말풍선입니다!</SpeechBubble>
-        <SpeechBubble role="answer">말풍선입니다!</SpeechBubble>
-        <SpeechBubble role="question">말풍선입니다!</SpeechBubble>
-        <SpeechBubble role="answer">말풍선입니다!</SpeechBubble>
-      </SpeechBubbleContainer>
-      <br></br>
-      <SelectContainer>
-        <Select>첫 번째 선택지 엄청 길어지는 경우 </Select>
-        <Select>두 번째 선택지가 이렇게 밀린다!</Select>
-      </SelectContainer>
-      <AppBlock>
-        <ProgressBar></ProgressBar>
-      </AppBlock>
-    </AppBlock>
+    <Router>
+      <Route exact path="/" component={Main}></Route>
+    </Router>
   );
 }
 
