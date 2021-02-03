ecefbda71f70c5d64578615d1d578d36547a9922
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-02-04 02:02:32 +0900

    Move SpeechBubbleContainer to MessagesContainer

--- frontend/src/components/Chat/Messages.js
+++ frontend/src/components/Chat/Messages.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
 import Message from './Message';
-import SpeechBubbleContainer from './SpeechBubbleContainer';
 
 const AlwaysScrollToBottom = () => {
   const scrollRef = useRef();
@@ -8,10 +8,10 @@ const AlwaysScrollToBottom = () => {
   return <div ref={scrollRef} />;
 };
 
-const Messages = ({ numbers }) => {
+const Messages = ({ numbers, selected }) => {
   return (
     <div className="messagesSection">
-      <SpeechBubbleContainer>
+      <MessagesContainer>
         {numbers.map((v) => {
           return (
             <div className="messagesContainer">
@@ -20,9 +20,27 @@ const Messages = ({ numbers }) => {
           );
         })}
         <AlwaysScrollToBottom />
-      </SpeechBubbleContainer>
+      </MessagesContainer>
     </div>
   )
 }
 
 export default Messages;
+
+const MessagesContainer = styled.div`
+  height: 30rem;
+  overflow-y: scroll;
+
+  /* scrollbar */
+  ::-webkit-scrollbar {
+    width: 16px;
+  }
+  ::-webkit-scrollbar-thumb {
+      height: 6px;
+      border: 4px solid rgba(0, 0, 0, 0);
+      background-clip: padding-box;
+      -webkit-border-radius: 7px;
+      background-color: #f1f3f5; // background: #dee2e6;
+      -webkit-box-shadow: inset -1px -1px 0px rgba(0, 0, 0, 0.05), inset 1px 1px 0px rgba(0, 0, 0, 0.05);
+  }
+`;
