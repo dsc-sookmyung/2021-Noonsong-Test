f635c9f29a8e843a1b9306b649e24221cf07f005
mori8 <kaithape@gmail.com>
2021-02-05 01:13:42 +0900

    Complete main page

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -3,70 +3,93 @@ import style from './Main.module.css';
 
 import ChatTemplate from '../Chat/ChatTemplate';
 import AboutTemplate from '../About/AboutTemplate';
-import ResultTemplate from '../Result/ResultTemplate';
+import BoardTemplate from '../Board/BoardTemplate';
 
 import testImg from '../../Images/frame-test.png';
 import aboutImg from '../../Images/frame-about.png';
 import bluePin from '../../Images/blue-pin.png';
+import tape from '../../Images/tape.png';
+import bedge from '../../Images/sookmyung-symbol.png';
+import noonsong from '../../Images/noonsong1.gif';
+
 
 function Main(props) {
-    const [isTestModalOpened, setIsTestModalOpened] = useState(false);
-    const [isAboutModalOpened, setIsAboutModalOpened] = useState(false);
-    const [isResultModalOpened, setIsResultModalOpened] = useState(false);
+  const [isTestModalOpened, setIsTestModalOpened] = useState(false);
+  const [isAboutModalOpened, setIsAboutModalOpened] = useState(false);
+  const [isBoardModalOpened, setIsBoardModalOpened] = useState(false);
 
-    const openTestModal = () => {
-        setIsTestModalOpened(true);
-        setIsAboutModalOpened(false);
-        setIsResultModalOpened(false);
-    }
+  const openTestModal = () => {
+    setIsTestModalOpened(true);
+    setIsAboutModalOpened(false);
+    setIsBoardModalOpened(false);
+  }
 
-    const closeTestModal = () => {
-        setIsTestModalOpened(false);
-    }
+  const closeTestModal = () => {
+    setIsTestModalOpened(false);
+  }
 
-    const openAboutModal = () => {
-        setIsTestModalOpened(false);
-        setIsAboutModalOpened(true);
-        setIsResultModalOpened(false);
-    }
+  const openAboutModal = () => {
+    setIsTestModalOpened(false);
+    setIsAboutModalOpened(true);
+    setIsBoardModalOpened(false);
+  }
 
-    const closeAboutModal = () => {
-        setIsAboutModalOpened(false);
-    }
+  const closeAboutModal = () => {
+    setIsAboutModalOpened(false);
+  }
 
-    const openResultModal = () => {
-        setIsAboutModalOpened(false);
-        setIsTestModalOpened(false);
-        setIsResultModalOpened(true);
-    }
+  const openBoardModal = () => {
+    setIsAboutModalOpened(false);
+    setIsTestModalOpened(false);
+    setIsBoardModalOpened(true);
+  }
 
-    const closeResultModal = () => {
-        setIsResultModalOpened(false);
-    }
+  const closeBoardModal = () => {
+    setIsBoardModalOpened(false);
+  }
 
-    return (
-        <div className={style.framewrapper}>
-                <div className={style.polaroid} onClick={openAboutModal}>
-                    <div className={style.imagewrapper}>
-                        <img src={aboutImg} alt="about" className={style.image}/>
-                        <img src={bluePin} alt="pin" className={style.pin}/>
-                    </div>
-                </div>
-                <div className={style.original} onClick={openTestModal}>
-                    <div className={style.imagewrapper}>
-                        <img src={testImg} alt="test" className={style.image}/>
-                    </div>
-                </div>
-                <div className={style.sticker} onClick={openResultModal}>
-                    <div>
-                        
-                    </div>
-                </div>
-                <ChatTemplate isOpened={isTestModalOpened} close={closeTestModal}/>
-                <AboutTemplate isOpened={isAboutModalOpened} close={closeAboutModal}/>
-                <ResultTemplate isOpened={isResultModalOpened} close={closeResultModal}/>
+  return (
+    <div className={style.contentwrapper}>
+      <div className={style.framewrapper}>
+        <div className={style.polaroid} onClick={openAboutModal}>
+          <div className={style.imagewrapper}>
+            <img src={aboutImg} alt="about" className={style.image}/>
+            <img src={bluePin} alt="pin" className={style.pin}/>
+          </div>
+          <div className={style.postit}>Gitribute 팀원 소개 →</div>
         </div>
-    );
+        <div className={style.mainpage}>
+          <h1 className={style.title}>
+            ❄️ 나는 어떤 타입의 <span className={style.highlight}>눈송이</span>일까? 
+          </h1>
+          <p className={style.desc + ' ' + style.typingtxt}>
+            n년차 눈송이인 당신! 당신은 어떤 타입의 눈송이인지 확인해보고 싶지 않으셨나요?<br/>
+            그런 당신을 위해 특별히 DSC Sookmyung의 Gitribute 팀에서 야심차게 눈송이 유형 테스트를 준비했습니다. 🥰<br/>
+            눈송이 유형 테스트를 통해 본인도 몰랐던 자신의 귀여움과 특별함을 찾아보세요!<br/><br/>
+            아래의 학교 사진을 눌러 테스트를 시작할 수 있답니다 👩🏻‍💻
+          </p>
+          <p className={style.typing}></p> 
+          <img src={noonsong} alt="noonsong" className={style.noonsong}/>
+        </div>
+        <div className={style.original} onClick={openTestModal}>
+          <div className={style.postit}>테스트 바로가기 →</div>
+            <div className={style.imagewrapper}>
+              <img src={testImg} alt="test" className={style.image}/>
+              <img src={tape} alt="tape" className={style.tape}/>
+            </div>
+        </div>
+        <div className={style.sticker} onClick={openBoardModal}>
+          <div className={style.bedgewrapper}>
+            <img src={bedge} alt="bedge" className={style.bedge + ' ' + style.image}/>
+          </div>
+          <div className={style.postit}>방명록 →</div>
+        </div>
+        <ChatTemplate isOpened={isTestModalOpened} close={closeTestModal}/>
+        <AboutTemplate isOpened={isAboutModalOpened} close={closeAboutModal}/>
+        <BoardTemplate isOpened={isBoardModalOpened} close={closeBoardModal}/>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
