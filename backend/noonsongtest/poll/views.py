9818b09b4f3db78574c82d749d077df1c51b413e
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-02-05 02:34:17 +0900

    Add Question and Answer in Poll App

--- backend/noonsongtest/poll/views.py
+++ backend/noonsongtest/poll/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets 
+from .serializers import QuestionSerializer, AnswerSerializer
+from .models import Question, Answer
 
-# Create your views here.
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset=Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset=Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+#get요청만 필요한데 일단 뭉뜽그려서 되나 확인위해 viewset으로 처리
