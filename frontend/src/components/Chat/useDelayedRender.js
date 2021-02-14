7b2ebe99bf4ff3bf1f2763ff859cd1284aae3571
Hee Suh <53266682+0hee0@users.noreply.github.com>
2021-02-14 21:57:44 +0900

    Delete frontend/src/components/Chat directory
    directory name을 Chat 에서 Test로 변경

--- frontend/src/components/Chat/useDelayedRender.js
+++
@@ -1,8 +0,0 @@
-const useDelayedRender = delay => {
-  const [delayed, setDelayed] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(() => setDelayed(false), delay);
-    return () => clearTimeout(timeout);
-  }, []);
-  return fn => !delayed && fn();
-};
