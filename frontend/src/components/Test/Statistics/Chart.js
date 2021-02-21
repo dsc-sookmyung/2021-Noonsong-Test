4c75f5118a08bcf373e8ed9ac400fe188222273d
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-02-21 21:40:31 +0900

    Add Chart

--- frontend/src/components/Test/Statistics/Chart.js
+++ frontend/src/components/Test/Statistics/Chart.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Doughnut } from 'react-chartjs-2';
 
-const Chart = () => {
+const options = {
+  layout: {
+    padding: {
+      bottom: 5,
+    }
+  },
+  legend: {
+    display: true,
+    position: "bottom",
+    align: "start",
+    fullWidth: true,
+    labels: {
+      padding: 12,
+      fontSize: 12,
+      usePointStyle: true,
+    }
+  },
+  maintainAspectRatio: false
+}
+
+const Chart = ({ info }) => {
+  let dataArr = [info.noonsong1_ratio, info.noonsong2_ratio, info.noonsong3_ratio, info.noonsong4_ratio,
+    info.noonsong5_ratio, info.noonsong6_ratio, info.noonsong7_ratio, info.noonsong8_ratio]
+  let rankColor = ["#a363d9","#ee657a","#db3838","#f6621f","#f9a228","#fecc2f","#b2c225","#33beb8"]
+  
+  const data = {
+    labels: [info.noonsong1_name, info.noonsong2_name, info.noonsong3_name, info.noonsong4_name,
+    info.noonsong5_name, info.noonsong6_name, info.noonsong7_name, info.noonsong8_name],
+    datasets: [
+      {
+        data: dataArr, 
+        backgroundColor: rankColor,
+        borderColor: rankColor,
+        borderWidth: 1,
+        hoverBackgroundColor: rankColor,
+        hoverBorderColor: rankColor
+      }
+    ]
+  };
+
   return (
     <Wrapper>
       <Title>
         차트
       </Title>
+      <ChartContainer>
+        <Doughnut
+          data={data}
+          options={options}
+        />
+      </ChartContainer>
     </Wrapper>
   )
 }
@@ -19,16 +65,22 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   padding: 1rem;
-  //overflow: scroll;
 `;
 
 const Title = styled.div`
   width: auto;
   height: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   padding-bottom: 1.2rem;
   font-weight: bold;
   font-size: 2rem;
+
+  /* Mobile */
+  @media only screen and (max-width: 767px) and (max-height: 700px) {
+    font-size: 1.6rem;
+  }
+`;
+
+const ChartContainer = styled.div`
+  width: 100%;
+  height: 20rem;
 `;
