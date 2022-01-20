e9819a1d6d9a70fe6e59d5f5bed2e751cef9453a
mori8 <kaithape@gmail.com>
2022-01-20 18:39:43 +0900

    Set environments for code refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Main from './components/Main/Main';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 // const pointColors = ['#4379B7', '#0D2D84']; // 연한 파랑, 코발트 블루
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Main}></Route>
-    </Router>
+    <BrowserRouter>
+      <Route path="/" element={<Main/>}></Route>
+    </BrowserRouter>
   );
 }
 
